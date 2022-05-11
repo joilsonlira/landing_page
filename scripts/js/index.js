224f355f795d5d7dbb90ac89ad44ebf21756dc66
joilsonlira <joilson.lira@live.com>
2022-05-10 23:14:35 -0300

    update

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -28,6 +28,6 @@ function showDivs(n) {
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" w3-red", "");
     }
-    x[slideIndex-1].style.display = "block";  
+    x[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " w3-red";
 }
