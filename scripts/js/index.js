4f08123cf8419e14ea8d66c757756334ae2b83de
joilsonlira <joilson.lira@live.com>
2022-05-12 22:44:41 -0300

    update: aplicado modal e atualizado estilos

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,33 +1,25 @@
+// Get the modal
+var modal = document.getElementById("myModal");
 
-const slideIndex = 1;
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-showDivs(slideIndex);
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-function plusDivs(n) {
-    showDivs(slideIndex += n);
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
 }
 
-function currentDiv(n) {
-    showDivs(slideIndex = n);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
 
-function showDivs(n) {
-    let i;
-    let x = document.getElementsByClassName("my_slides");
-    let dots = document.getElementsByClassName("demo");
-
-    if (n > x.length){
-        slideIndex = 1
-    }    
-    if (n < 1){
-        slideIndex = x.length
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+    modal.style.display = "none";
     }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" w3-red", "");
-    }
-    x[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " w3-red";
 }
