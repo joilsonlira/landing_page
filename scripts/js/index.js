e445fde3290b28dd0c583d6c442df03609226347
joilsonlira <joilson.lira@live.com>
2022-05-15 17:52:12 -0300

    update: adicionado link ao botão de endereço do seminario. Adicionado slide na area de sobre a ABiogás

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,4 +1,4 @@
-
+//MODAL SCRIPT
 function open_modal(modal_name){
     let modal = document.getElementById(modal_name);
     console.log(modal)
@@ -16,47 +16,18 @@ function close_modal(modal_name){
         modal.style.display = 'none';
 }
 
+//SLIDE SCRIPT
+var count = 1;
+document.getElementById("radio1").checked = true;
 
+setInterval( function(){
+    next_slide();
+}, 10000);
 
-
-
-
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-// var modal_contato = document.getElementById("myModal_contato");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-// var btn_contato = document.getElementById("btn_contato");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-// var span_contato = document.getElementsByClassName("close_contato")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//     modal.style.display = "block";
-// }
-// btn_contato.onclick = function() {
-//     modal_contato.style.display = "block";
-// }
-
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//     modal.style.display = "none";
-// }
-// span_contato.onclick = function() {
-//     modal_contato.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-//     if(event.target == modal_contato){
-//         modal_contato.style.display = "none";
-//     }
-// }
+function next_slide(){
+    count++;
+    if(count > 2){
+        count = 1;
+    }
+document.getElementById("radio" + count).checked = true;
+}
